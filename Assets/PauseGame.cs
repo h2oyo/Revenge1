8dba60d97c101de838918aa562aea334ed4e7cb6
Izaac Knien <izaac.knien@students.aie.edu.au>
2016-11-18 09:53:23 -0800

    PaueMenu
    Just needs to be changed to accure upon win State

---
+++ Assets/PauseGame.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseGame : MonoBehaviour {
+    public Transform canvas;
+	
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(canvas.gameObject.activeInHierarchy == false) { canvas.gameObject.SetActive(true); }
+            else { canvas.gameObject.SetActive(false); }
+        }
+
+      
+
+
+	}
+
+   public void nextLevel()
+    {
+        print("nextLevel");
+    }
+
+    public void MainMenu()
+    {
+        print("MainMEnu");
+    }
+
+}
